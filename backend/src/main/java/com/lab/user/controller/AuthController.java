e714d8dc3bf02a16523cb2da7f4f9a7cc05802bb
choiseoyeon67 <tjdusuny2@gmail.com>
2026-06-13 21:53:36 +0900

    Update AuthController.java

--- backend/src/main/java/com/lab/user/controller/AuthController.java
+++ backend/src/main/java/com/lab/user/controller/AuthController.java
@@ -5,7 +5,7 @@ import com.lab.user.service.AuthService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
-@RestController @RequiredArgsConstructor @RequestMapping("/api/auth")
+@RestController @RequiredArgsConstructor @RequestMapping("/apiㅇ/auth")
 public class AuthController {
     private final AuthService authService;
     @PostMapping("/signup") public ApiResponse<Void> signup(@Valid @RequestBody SignupRequest r){authService.signup(r); return ApiResponse.ok("회원가입 신청이 완료되었습니다.");}
